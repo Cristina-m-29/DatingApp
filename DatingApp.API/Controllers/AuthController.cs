5a3481af253be3068afec9c367a2dc58c3afcdd9
Mititelu Cristina <61682864+Cristina-m-29@users.noreply.github.com>
2020-07-02 18:35:27 +0300

    end of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -46,6 +46,7 @@ namespace DatingApp.API.Controllers
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto){
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
             if(userFromRepo == null)
                 return Unauthorized();
@@ -71,7 +72,7 @@ namespace DatingApp.API.Controllers
 
             return Ok(new {
                 token =  tokenHandler.WriteToken(token)
-            });
+            });            
         }
     }
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using DatingApp.API.Data;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API
 {
@@ -51,6 +55,20 @@ namespace DatingApp.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else{
+
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if(error != null){
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             // app.UseHttpsRedirection();
